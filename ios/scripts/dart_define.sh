b6acdd731531367026373d20cd76a85d1e45b597
Yamasaki-pan961 <atusi08250522@gmail.com>
2022-05-19 13:51:49 +0900

    chore: Dart-Defineのパースを修正

--- ios/scripts/dart_define.sh
+++ ios/scripts/dart_define.sh
@@ -1,9 +1,25 @@
-#/bin/bash
+#!/bin/sh
 
-echo $DART_DEFINE | tr ',' '\n' | while read line; 
+# 作成ファイルパス
+OUTPUT_FILE="${SRCROOT}/Flutter/DartDefine.xcconfig"
+
+# Flutter 2.2 以降で必要な、Dart-Defineのデコード処理 
+function decode_url() { echo "${*}" | base64 --decode; }
+
+# あらかじめファイル内容を空にする
+: > $OUTPUT_FILE
+
+IFS=',' read -r -a define_items <<<"$DART_DEFINES"
+
+for index in "${!define_items[@]}"
 do
-  echo $line | base64 -d | tr ',' '\n' | xargs -I@ bash -c "echo @ | grep 'FLAVOR' | sed 's/.*=//'"
-done | (
-  read flavor
-  echo "#include \"$flavor.xcconfig\"" > $SRCROOT/Flutter/DartDefine.xcconfig
-)
+    # Flutter 2.2 以降で必要なデコードを実行する
+    item=$(decode_url "${define_items[$index]}")
+    # FLAVORが含まれるDart Defineの場合
+    if [ $(echo $item | grep 'FLAVOR') ] ; then
+        # FLAVORの値(=の右側)
+        value=${item#*=}
+        # FLAVORに対応したXCConfigファイルをincludeさせる
+        echo "#include \"$value.xcconfig\"" >> $OUTPUT_FILE
+    fi
+done
