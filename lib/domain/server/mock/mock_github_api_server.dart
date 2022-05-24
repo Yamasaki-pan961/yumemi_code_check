584f229259b1c2b161aa2d97de914fd12aca90db
Yamasaki-pan061 <atusi08250522@gmail.com>
2022-05-24 10:29:24 +0900

    feat: サーバーMockにレスポンスを設定できるメソッドを追加

--- lib/domain/server/mock/mock_github_api_server.dart
+++ lib/domain/server/mock/mock_github_api_server.dart
@@ -1,42 +1,91 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:yumemi_code_check/domain/server/client/github_api_path.dart';
-import 'package:yumemi_code_check/domain/server/mock/search_repository_json.dart';
 
 class MockGithubApiServer {
   MockGithubApiServer() {
     dio = Dio(BaseOptions(baseUrl: githubApiBaseUrl));
     _dioAdapter = DioAdapter(dio: dio);
-    _setSearchRepositoryResponse();
   }
 
   late final Dio dio;
   late final DioAdapter _dioAdapter;
 
-  void _setSearchRepositoryResponse() {
-    const path = githubSearchRepositoriesUrl;
-    
-    _dioAdapter.onGet(
-      path,
-      (server) async {
-        server.reply(
-          200,
-          json.decode(searchRepositoryJson) as Map<String, dynamic>,
+  void onGetSuccessStatuses({
+    required String path,
+    required Map<int, Map<String, dynamic>> responseMap,
+    required Map<int, VoidCallback> statusFunction,
+  }) {
+    statusFunction.forEach((status, function) {
+      if (responseMap.containsKey(status)) {
+        _dioAdapter.onGet(
+          path,
+          (server) => server.reply(status, responseMap[status]),
+          data: Matchers.any,
         );
-      },
-      data: Matchers.any,
-    );
+        function();
+      }
+    });
   }
 
-  void statusPatternTest({
+  void onGetFailureStatus({
     required String path,
-    VoidCallback? code200,
-    VoidCallback? code304,
-    VoidCallback? code403,
-    VoidCallback? code422,
-    VoidCallback? code503,
-  }) {}
+    required Map<int, Map<String, dynamic>> responseMap,
+    required Map<int, VoidCallback> statusFunction,
+  }) {
+    statusFunction.forEach((status, function) {
+      if (responseMap.containsKey(status)) {
+        _dioAdapter.onGet(
+          path,
+          (server) => server.throws(
+            status,
+            DioError(
+              requestOptions: RequestOptions(path: path),
+              response: Response<dynamic>(
+                requestOptions: RequestOptions(
+                  path: path,
+                  data: responseMap[status],
+                ),
+                statusCode: status,
+              ),
+              type: DioErrorType.response,
+            ),
+          ),
+          data: Matchers.any,
+        );
+        function();
+      }
+    });
+  }
+
+  void setGetSuccessResponse(String path, int status, dynamic data) =>
+      _dioAdapter.onGet(
+        path,
+        (server) => server.reply(status, data),
+        data: Matchers.any,
+      );
+
+  void setGetFailureResponse(String path, int status, dynamic data) =>
+      _dioAdapter.onGet(
+        path,
+        (server) => server.throws(
+          status,
+          DioError(
+            requestOptions: RequestOptions(path: path),
+            response: Response<dynamic>(
+              requestOptions: RequestOptions(
+                path: path,
+                data: data,
+              ),
+              statusCode: status,
+            ),
+            type: DioErrorType.response,
+          ),
+        ),
+        data: Matchers.any,
+      );
+
+  void close() => _dioAdapter.close();
+  void reset() => _dioAdapter.reset();
 }
