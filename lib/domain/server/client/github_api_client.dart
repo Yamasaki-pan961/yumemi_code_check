0196afd2940158d09efb003d221edf005c690828
Yamasaki-pan061 <atusi08250522@gmail.com>
2022-05-24 12:03:40 +0900

    fix: hideを使いambiguouse_importを回避するように変更

--- lib/domain/server/client/github_api_client.dart
+++ lib/domain/server/client/github_api_client.dart
@@ -1,5 +1,5 @@
 import 'package:retrofit/retrofit.dart';
-import 'package:dio/dio.dart' as dio;
+import 'package:dio/dio.dart' hide Headers;
 import 'package:yumemi_code_check/domain/server/client/github_api_path.dart';
 import '../../model/github/search_repository_response.dart';
 
@@ -8,7 +8,7 @@ part '../../../generated/domain/server/client/github_api_client.g.dart';
 /// Retrofitを使ったGithub API Client
 @RestApi(baseUrl: githubApiBaseUrl)
 abstract class GithubApiClient {
-  factory GithubApiClient(dio.Dio dio, {String baseUrl}) = _GithubApiClient;
+  factory GithubApiClient(Dio dio, {String baseUrl}) = _GithubApiClient;
 
   @GET(githubSearchRepositoriesUrl)
   @Headers(<String, dynamic>{'accept': 'application/vnd.github.v3+json'})
