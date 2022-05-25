d34ebe8d69f86be4004237853f4f44fbf7a43757
Yamasaki-pan061 <atusi08250522@gmail.com>
2022-05-25 11:46:48 +0900

    feat: レスポンス表示とエラー表示ができるようになった

--- lib/ui/page/search_page.dart
+++ lib/ui/page/search_page.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:quiver/strings.dart';
+import 'package:yumemi_code_check/domain/model/github/search_repository_response.dart';
+import 'package:yumemi_code_check/domain/model/result.dart';
 import 'package:yumemi_code_check/l10n/l10n.dart';
+import 'package:yumemi_code_check/ui/view_model/github_api_repository_provider.dart';
+import 'package:yumemi_code_check/ui/widget/search_failure.dart';
+import 'package:yumemi_code_check/ui/widget/search_success.dart';
 
 class SearchPage extends HookConsumerWidget {
   const SearchPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final loadingState = useState(false);
+    final resultState = useState<Result<SearchRepositoryResponse>?>(null);
     final formKey = GlobalKey<FormState>();
+
     void onSubmit() {
       if (formKey.currentState!.validate()) {
         formKey.currentState!.save();
@@ -26,7 +35,15 @@ class SearchPage extends HookConsumerWidget {
             child: Form(
               key: formKey,
               child: TextFormField(
-                onSaved: (value) {},
+                onSaved: (value) async {
+                  if (value != null) {
+                    loadingState.value = true;
+                    resultState.value = await ref
+                        .watch(githubApiRepositoryProvider)
+                        .searchRepository(value);
+                    loadingState.value = false;
+                  }
+                },
                 validator: (value) {
                   if (isBlank(value)) {
                     return L10n.of(context)!.blankMessage;
@@ -52,6 +69,19 @@ class SearchPage extends HookConsumerWidget {
               ),
             ),
           ),
+          if (loadingState.value) const CircularProgressIndicator(),
+          Builder(
+            builder: (context) {
+              final result = resultState.value;
+              if (result is Success<SearchRepositoryResponse>) {
+                return SearchSuccess(success: result);
+              }
+              if (result is Failure<SearchRepositoryResponse>) {
+                return SearchFailure(failure: result);
+              }
+              return Container();
+            },
+          )
         ],
       ),
     );
