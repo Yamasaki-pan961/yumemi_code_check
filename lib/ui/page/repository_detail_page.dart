8f535eafece1ea1f1af3768b9aa6d2e992708fed
Yamasaki-pan061 <atusi08250522@gmail.com>
2022-05-25 12:35:54 +0900

    feat: 画像表示の実装

--- lib/ui/page/repository_detail_page.dart
+++ lib/ui/page/repository_detail_page.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:yumemi_code_check/domain/model/github/github_repository_info.dart';
 
 class RepositoryDetailPage extends StatelessWidget {
-  const RepositoryDetailPage({Key? key}) : super(key: key);
+  const RepositoryDetailPage({Key? key, required this.info}) : super(key: key);
 
+  final GithubRepositoryInfo info;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(info.name, style: Theme.of(context).textTheme.bodyMedium),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24),
+              child: SizedBox(
+                height: 250,
+                width: 250,
+                child: Container(
+                  color: Theme.of(context).inputDecorationTheme.fillColor,
+                  child: Image.network(
+                    info.owner.avatarUrl,
+                    errorBuilder: (context, error, st) => Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: const [
+                        Text(
+                          'Image Load Error',
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/ui/widget/search_success.dart
+++ lib/ui/widget/search_success.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yumemi_code_check/domain/model/github/github_repository_info.dart';
 import 'package:yumemi_code_check/domain/model/github/search_repository_response.dart';
 import 'package:yumemi_code_check/domain/model/result.dart';
+import 'package:yumemi_code_check/ui/page/repository_detail_page.dart';
 
 class SearchSuccess extends StatelessWidget {
   const SearchSuccess({Key? key, required this.success}) : super(key: key);
@@ -21,8 +22,18 @@ class SearchSuccess extends StatelessWidget {
             Expanded(
               child: ListView.builder(
                 itemBuilder: (context, index) {
+                  final info = items[index];
                   return ListTile(
-                    subtitle: Text(items[index].name),
+                    subtitle: Text(info.name),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute<Widget>(
+                          builder: (context) =>
+                              RepositoryDetailPage(info: info),
+                        ),
+                      );
+                    },
                   );
                 },
                 itemCount: items.length,
