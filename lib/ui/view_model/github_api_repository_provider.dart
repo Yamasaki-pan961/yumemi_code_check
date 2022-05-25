34189ae2df167608b2dccec849c5db52704d2283
Yamasaki-pan061 <atusi08250522@gmail.com>
2022-05-25 11:46:00 +0900

    feat: mockのレスポンスを返すようにした

--- lib/ui/view_model/github_api_repository_provider.dart
+++ lib/ui/view_model/github_api_repository_provider.dart
@@ -2,7 +2,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:yumemi_code_check/domain/repository/github/github_api_repository.dart';
 import 'package:yumemi_code_check/domain/repository/github/github_api_repository_impl.dart';
 import 'package:yumemi_code_check/domain/server/client/github_api_client.dart';
+import 'package:yumemi_code_check/domain/server/client/github_api_path.dart';
 import 'package:yumemi_code_check/domain/server/mock/mock_github_api_server.dart';
+import 'package:yumemi_code_check/domain/server/mock/search_repository_json.dart';
 
 final flavorProvider =
     Provider.autoDispose((ref) => const String.fromEnvironment('FLAVOR'));
@@ -10,7 +12,13 @@ final flavorProvider =
 final mockServerProvider = Provider.autoDispose((ref) {
   final flavor = ref.watch(flavorProvider);
   if (flavor == 'dev') {
-    return MockGithubApiServer();
+    final mock = MockGithubApiServer()
+      ..setGetSuccessResponse(
+        githubSearchRepositoriesUrl,
+        200,
+        mockSearchStatusResponse[200],
+      );
+    return mock;
   }
   return null;
 });
