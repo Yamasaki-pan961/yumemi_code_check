4e9c33a5698228ea1bdbcf958a0d024d9ad36ece
Yamasaki-pan061 <atusi08250522@gmail.com>
2022-05-25 13:20:13 +0900

    feat: 情報の表示を実装

--- lib/ui/page/repository_detail_page.dart
+++ lib/ui/page/repository_detail_page.dart
@@ -9,32 +9,90 @@ class RepositoryDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(info.name, style: Theme.of(context).textTheme.bodyMedium),
+        title: Text(
+          info.name,
+          style: Theme.of(context).primaryTextTheme.titleSmall,
+        ),
       ),
       body: Center(
-        child: Column(
+        child: ListView(
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 24),
-              child: SizedBox(
-                height: 250,
-                width: 250,
-                child: Container(
-                  color: Theme.of(context).inputDecorationTheme.fillColor,
-                  child: Image.network(
-                    info.owner.avatarUrl,
-                    errorBuilder: (context, error, st) => Stack(
-                      alignment: AlignmentDirectional.center,
-                      children: const [
-                        Text(
-                          'Image Load Error',
-                        )
-                      ],
+            Column(
+              children: [
+                SizedBox(
+                  height: 250,
+                  width: 250,
+                  child: Container(
+                    color: Theme.of(context).inputDecorationTheme.fillColor,
+                    child: Image.network(
+                      info.owner.avatarUrl,
+                      errorBuilder: (context, error, st) => Stack(
+                        alignment: AlignmentDirectional.center,
+                        children: const [
+                          Text(
+                            'Image Load Error',
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            )
+                Table(
+                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                  columnWidths: const {
+                    0: FractionColumnWidth(0.3),
+                    1: FractionColumnWidth(0.2)
+                  },
+                  children: [
+                    TableRow(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text('Language:'),
+                        ),
+                        Text(info.language.toString()),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text('Stars:'),
+                        ),
+                        Text(info.stargazersCount.toString()),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text('Forks:'),
+                        ),
+                        Text(info.forksCount.toString()),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text('Open Issues:'),
+                        ),
+                        Text(info.openIssuesCount.toString()),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text('Watchers:'),
+                        ),
+                        Text(info.watchersCount.toString()),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
           ],
         ),
       ),
